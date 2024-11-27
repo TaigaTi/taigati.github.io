11e1c9e3cfeddd9fe743857af0c68110abb9cfd9
TaigaTi <tarikabirch@gmail.com>
2024-11-27 19:59:57 -0400

    Created error page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {  Routes, Route, HashRouter, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Error from './components/Error';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/*" element={<Error />} />
         </Routes>
       </BrowserRouter>
 
