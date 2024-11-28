c820dc078a45aeb250fc769f67c21ce50385eaf8
TaigaTi <tarikabirch@gmail.com>
2024-11-27 20:03:50 -0400

    Fixed app background

--- src/components/Error.js
+++ src/components/Error.js
@@ -2,10 +2,12 @@ import '../styles/Error.css'
 
 function Error() {
     return (
-        <div className="flex flex-col items-center">
+        <div className="error flex flex-col items-center">
             <img src="assets/error.gif" alt="404" width={900}/>
             <h1 className="text-8xl" style={{color: "#38b6ff"}}>404</h1>
             <p className="text-2xl opacity-80">Page not found</p>
+
+            <button className="text-lg resume-button rounded mt-5 px-6 py-1 flex gap-2 items-center hover:scale-105 ease-in" onClick={() => window.location.replace("/")}>Return Home</button>
         </div>
     )
 }  
