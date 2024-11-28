62ddc24a1df1488a43405d923b922930071542c5
TaigaTi <tarikabirch@gmail.com>
2024-11-27 20:15:38 -0400

    Added layout to projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,28 @@
+import ProjectPreview from "./ProjectPreview";
+import '../styles/Projects.css'
+
 function Projects() {
     return (
-        <div>
-            <h1>Projects</h1>
+        <div className="projects flex flex-col items-center pt-10 min-h-[90vh]">
+            <h1 className="text-3xl md:text-4xl mb-1 pt-10">Projects</h1>
+
+            <div className="search flex flex-col md:flex-row items-center gap-4 p-8">
+              <input className="search-input" placeholder="Search Projects"/>
+            </div>
+
+            <div className="projects-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8">
+                <ProjectPreview />
+                <ProjectPreview />
+                <ProjectPreview />
+                <ProjectPreview />
+                <ProjectPreview />
+                <ProjectPreview />
+                <ProjectPreview />
+                <ProjectPreview />
+                <ProjectPreview />
+            </div>
+
+
         </div>
     );
 }
