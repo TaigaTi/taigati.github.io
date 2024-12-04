be83282b1226bdc09a72ca9862223503f7c563d3
TaigaTi <tarikabirch@gmail.com>
2024-12-04 06:36:31 -0400

    Fixed spacing of project tags on project preview card

--- src/components/ProjectPreview.js
+++ src/components/ProjectPreview.js
@@ -1,16 +1,18 @@
 import '../styles/ProjectPreview.css'
 import projects from '../data/projects'
 
-function ProjectPreview({id}) {
+function ProjectPreview({ id }) {
     const project = projects.find((project) => project.id === id)
 
     return (
         <>
-            <a className="project-preview text-start" href={project.link || project.repo} >
-                <img className="project-image" src={project.image} alt={project.title} />
-                <div className='p-6'>
-                    <h1 className="text-lg mb-2">{project.title}</h1>
-                    <p className="text-sm opacity-80">{project.shortDescription}</p>
+            <a className="project-preview container text-start" href={project.link || project.repo} >
+                <img className="project-image h-1/2" src={project.image} alt={project.title} />
+                <div className='project-info h-1/2 container p-6  pb-8 flex flex-col justify-evenly'>
+                    <div className="project-text pb-1">
+                        <h1 className="text-lg mb-2">{project.title}</h1>
+                        <p className="text-sm opacity-80">{project.shortDescription}</p>
+                    </div>
                     <div className="project-tags flex gap-2 mt-3 text-sm flex-wrap">
                         {project.tags.map((tag) => {
                             return (
