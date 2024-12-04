f92d894640a9adca8da72ab89c7f1aef53ea3766
TaigaTi <tarikabirch@gmail.com>
2024-12-04 06:38:14 -0400

    Fixed padding for project preview details text

--- src/components/ProjectPreview.js
+++ src/components/ProjectPreview.js
@@ -8,7 +8,7 @@ function ProjectPreview({ id }) {
         <>
             <a className="project-preview container text-start" href={project.link || project.repo} >
                 <img className="project-image h-1/2" src={project.image} alt={project.title} />
-                <div className='project-info h-1/2 container p-6  pb-8 flex flex-col justify-evenly'>
+                <div className='project-info h-1/2 container p-6 pt-4 pb-8 flex flex-col justify-evenly'>
                     <div className="project-text pb-1">
                         <h1 className="text-lg mb-2">{project.title}</h1>
                         <p className="text-sm opacity-80">{project.shortDescription}</p>
