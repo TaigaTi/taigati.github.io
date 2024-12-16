7e980737ec92d7dca15784a830d92cbf92d69ca4
TaigaTi <tarikabirch@gmail.com>
2024-12-16 07:29:13 -0400

    Added design card info and images

--- src/components/Designs.js
+++ src/components/Designs.js
@@ -4,7 +4,10 @@ import DesignCard from "./DesignCard.js"
 function Designs() {
     return (
         <div className="projects flex flex-col items-center pb-10 sm:p-10 min-h-[100vh]">
-            <h1 className="text-3xl md:text-4xl pt-10">Bringing Ideas To Life</h1>
+            <div className="pb-2">
+                <h1 className="text-3xl md:text-5xl pt-10">Bringing Visions To Life</h1>
+                <p className="pt-2">Explore my work in graphic design, mobile design, web design, branding, and more.</p>
+            </div>
 
             <div className="projects-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-12 pt-5">
                 {designs.map((design) => {
